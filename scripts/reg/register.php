4c16bf018b89d6c79f54b426f2e28692aedb0e00
Rumynoff <76729345+Rumynoff@users.noreply.github.com>
2024-05-05 17:49:32 +0300

    Add files via upload

--- scripts/reg/register.php
+++ scripts/reg/register.php
@@ -8,6 +8,7 @@ if (isset($_POST['submit'])) {
     $email = mysqli_real_escape_string($db_connect, $_POST['email']);
     $pass = mysqli_real_escape_string($db_connect, $_POST['pass']);
     $pass2 = mysqli_real_escape_string($db_connect, $_POST['pass2']);
+    $referral_code = mysqli_real_escape_string($db_connect, $_POST['referral_code']);
 
     // Validate input data
     if (empty($email)) {
@@ -41,23 +42,35 @@ if (isset($_POST['submit'])) {
         }
     }
 
+    // Check if referral code is valid
+    $sql = "SELECT id FROM users WHERE referral_code = '$referral_code'";
+    $res = mysqli_query($db_connect, $sql);
+
+    if (mysqli_num_rows($res) == 0) {
+        $err[] = 'Invalid referral code!';
+    }
+
     // Generate salt and hash password
     $salt = substr(md5(uniqid()), -8);
     $pass = md5(md5($pass). $salt);
     $referral = substr(md5('$hot'. uniqid()), 0, 12);
-    $referral_code = '$hot'.$referral;
+    $new_referral_code = '$hot'.$referral;
 
     $activation_key = md5($salt);
     $activation_expiry = time() + 60 * 60; // expiry time is 1 hour from now
 
-// Insert user data into the database
+    // Insert user data into the database
     if (empty($err)) {
-        $sql = "INSERT INTO users (login, pass, salt, active_hex, confirm_token, confirm_expiry, status, referral_code) VALUES ('$email', '$pass', '$salt', '', '$activation_key', '$activation_expiry', 0, '$referral_code')";
+        $sql = "INSERT INTO users (login, pass, salt, active_hex, confirm_token, confirm_expiry, status, referral_code) VALUES ('$email', '$pass', '$salt', '', '$activation_key', '$activation_expiry', 0, '$new_referral_code')";
         $res = mysqli_query($db_connect, $sql);
 
         if (!$res) {
             $err[] = 'Error inserting data into the database: '. mysqli_error($db_connect);
         } else {
+            // Update the referrer's points
+            $sql = "UPDATE users SET points = points + 1 WHERE referral_code = '$referral_code'";
+            mysqli_query($db_connect, $sql);
+
             // Redirect to success page
             header('Location:..\..\success.html');
             exit;
@@ -73,3 +86,4 @@ if (isset($_POST['submit'])) {
         echo '</ul>';
     }
 }
+?>
